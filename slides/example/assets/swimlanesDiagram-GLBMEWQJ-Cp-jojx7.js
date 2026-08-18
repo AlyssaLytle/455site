b6e178ef286e595f5a1bba5e48c64c920af43d58
Alyssa Lytle <alyssalytle@Alyssas-MacBook-Pro.local>
2026-08-18 10:45:32 -0400

    Try single slidedeck deployment

--- slides/example/assets/swimlanesDiagram-GLBMEWQJ-Cp-jojx7.js
+++
@@ -1,8 +0,0 @@
-import{r as e}from"./chunk-PTVI3W5X-DVfdoxm6.js";import"./chunk-FO5PYUIK-CPzj2963.js";import"./chunk-NEZ6ONQZ-BzmD4JGr.js";import"./chunk-PWCFYZI5-Dz1excih.js";import"./chunk-PLCLPJVV-BfMyJHbe.js";import"./chunk-XXV7AQMY-D1xEw2T_.js";import"./chunk-IPM4HZQ6-CQmHNgJe.js";import"./chunk-OX6ZKUVE-6FxE-lH0.js";import"./chunk-YV36FAQU-D5VHxcwN.js";import"./chunk-AQ2D6KKF-_vRnD-Fb.js";import"./chunk-2V3FENDJ-BQTQ9Xbu.js";import"./chunk-DKH7L6O3-DaXkYa1J.js";import"./chunk-RTI7CJYH-CrTK6TD7.js";import"./chunk-MPM6TOF7-6VPqhxel.js";import"./chunk-STSWKLMO-BvrgBoIk.js";import"./chunk-ZICDAICJ-CChi2iwO.js";import"./chunk-HPLX5OYV-zid2YVw5.js";import"./chunk-BYAF6FXG-g8KOZX2C.js";import{r as t,t as n}from"./chunk-MX3EQCGM-DlDtUEOk.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
